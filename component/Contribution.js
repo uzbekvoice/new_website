c5a335ba9ae392de1b228794b6e414c297abb44c
Abulfayz <abulfayzmamatqulov@gmail.com>
2022-05-30 21:33:02 +0500

    Contributions.js

---
+++ component/Contribution.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '../styles/Contribution.module.css'
+export default function Contribution() {
+  return (
+    <div className={styles.contribution}>
+    <div className={styles.content}>
+
+    </div>
+    </div>
+  )
+}
