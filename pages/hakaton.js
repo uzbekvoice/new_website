0256b1f79578d6fc695b8321283afba8fe25716f
Abulfayz <abulfayzmamatqulov@gmail.com>
2022-05-31 01:19:54 +0500

    Hide Navbar hackaton

--- pages/hakaton.js
+++ pages/hakaton.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import HeaderHakataon from '../component/HeaderHakaton'
 
 export default function hakaton() {
   return (
     <div>
+   {/* <HeaderHakataon /> */}
         <h1>This hakaton page</h1>
     </div>
   )
