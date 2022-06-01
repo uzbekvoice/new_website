ac56c212b55cb867537217f154dbf9d1b937026f
shokh001 <shohjahondilmurodov56@gmail.com>
2022-06-01 16:32:16 +0300

    navbar and footer is responsive

--- component/HakatonHero.js
+++ component/HakatonHero.js
@@ -4,24 +4,26 @@ import styles from '../styles/HakatonHero.module.css'
 
 export default function HakatonHero() {
     return (
-        <div className={styles.hakatonHeroWrappper + ' container__fluid'}>
-            <HeaderHakaton />
-            <div className={styles.hakatonHero}>
-                <h3 className={styles.hakatonHero_Left}>
-                    Hakaton
-                </h3>
-                <div className={styles.hakatonHero_Right}>
-                    <div>
-                        <h4>350</h4>
-                        <p>Qatnashchilar</p>
-                    </div>
-                    <div>
-                        <h4>6</h4>
-                        <p>Jamoa</p>
-                    </div>
-                    <div>
-                        <h4>6</h4>
-                        <p>Loyiha</p>
+        <div className={styles.hakatonHeroWrappper}>
+            <div className='container__fluid'>
+                <HeaderHakaton />
+                <div className={styles.hakatonHero}>
+                    <h3 className={styles.hakatonHero_Left}>
+                        Hakaton
+                    </h3>
+                    <div className={styles.hakatonHero_Right}>
+                        <div>
+                            <h4>350</h4>
+                            <p>Qatnashchilar</p>
+                        </div>
+                        <div>
+                            <h4>6</h4>
+                            <p>Jamoa</p>
+                        </div>
+                        <div>
+                            <h4>6</h4>
+                            <p>Loyiha</p>
+                        </div>
                     </div>
                 </div>
             </div>
