b1677fa015cbfffabd7a5597261819616d3b6114
shokh001 <shohjahondilmurodov56@gmail.com>
2022-06-07 01:47:00 +0300

    yangilikById page qilindi va responsive

--- component/OurTeam.js
+++ component/OurTeam.js
@@ -16,40 +16,40 @@ const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
   ssr: false,
 });
 export default function OurTeam() {
-    const Responsive ={
-        0:{
-          items:1.2,
-          margin:5,
-        },
+  const Responsive = {
+    0: {
+      items: 1.2,
+      margin: 5,
+    },
 
-        580:{
-            items:2,
-            margin:10,
-        },
-        
-        900:{
-            items:3,
-            margin:20,
-        },
+    580: {
+      items: 2,
+      margin: 10,
+    },
 
-        1200:{
-            items:4,
-            margin:20,
-        },
-        
-        1400:{
-            items:4,
-            margin:10,
-        }
+    900: {
+      items: 3,
+      margin: 20,
+    },
+
+    1200: {
+      items: 4,
+      margin: 20,
+    },
+
+    1400: {
+      items: 4,
+      margin: 10,
     }
+  }
   return (
     <div className={styles.ourteam}>
-         <h3>Uzbekvoice Jamoasi</h3>
-         <div className={styles.sliders}>
+      <h3>Uzbekvoice Jamoasi</h3>
+      <div className={styles.sliders}>
         <OwlCarousel className={styles.slider} loop margin={10} responsive={Responsive} autoplay autoplayTimeout={3000}>
-          <div className={styles.item} >
+          <div className={styles.item}>
             <Image src="/team1.png" width={296} height={296} alt="partners1" />
-             <h4>Courtney Henry</h4>
+            <h4>Courtney Henry</h4>
           </div>
           <div className={styles.item}>
             <Image src="/team2.png" width={296} height={296} alt="partners2" />
@@ -63,7 +63,6 @@ export default function OurTeam() {
             <Image src="/team4.png" width={296} height={296} alt="partners4" />
             <h4>Courtney Henry</h4>
           </div>
-       
         </OwlCarousel>
       </div>
     </div>
--- component/Yangiliklar/YangilikSection.js
+++ component/Yangiliklar/YangilikSection.js
@@ -4,29 +4,36 @@ import s from "../../styles/Yangiliklar.module.css";
 
 const YangilikSection = () => {
   return (
-      <section className={s.YangilikSection}>
-        <div className={s.container}>
-            <h1 className={s.main}>Yangiliklar</h1>
-            <div className={s.gradientCircle}></div>
-            <div className={s.gradientCircle2}></div>
-            <div className={s.gradientCircle3}></div>
-          <div className={s.parent}>
-            {data.map(function (el) {
-              return (
-                <div className={s.box} key={el.id}>
-                  <img src={el.image} alt="image" key={el.id}/>
+    <section className={s.YangilikSection}>
+      <div className={s.container}>
+        <h1 className={s.main}>Yangiliklar</h1>
+        {/* <div className={s.gradientCircle}></div>
+        <div className={s.gradientCircle2}></div>
+        <div className={s.gradientCircle3}></div> */}
+        <div className={s.parent}>
+          {data.map(function (el) {
+            return (
+              <div className={s.box} key={el.id}>
+                <div className={s.boxHeader}>
+                  <img src={el.image} alt="image" key={el.id} />
+                </div>
+                <div className={s.boxBody}>
                   <h3 className={s.title}>{el.title}</h3>
                   <h5 className={s.desc}>{el.desc}</h5>
                   <div className={s.flexClass}>
-                      <a href="#">{el.btn}</a>
-                      <p className={s.ppp}>{el.date}</p>
+                    <a href="#">
+                      Узнать больше
+                      <img src='/chevron-right.svg' />
+                    </a>
+                    <p className={s.ppp}>{el.date}</p>
                   </div>
                 </div>
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
         </div>
-      </section>
+      </div>
+    </section>
   );
 };
 
