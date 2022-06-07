279e60d15cc48125ca171a8e33928f8d82d85a64
Abulfayz <abulfayzmamatqulov@gmail.com>
2022-06-07 08:24:32 +0500

    Step mobile responsive

--- pages/hakaton.js
+++ pages/hakaton.js
@@ -2,11 +2,13 @@ import React from 'react'
 import HakatonFoto from '../component/HakatonFoto'
 import HakatonJuri from '../component/HakatonJuri'
 import HakatonTeams from '../component/HakatonTeams'
+import InitiativePartners from '../component/InitiativePartners'
 
 export default function hakaton() {
   
   return (
     <>
+    <InitiativePartners />
       <HakatonTeams />
       <HakatonFoto />
       <HakatonJuri />
--- pages/yangilik.js
+++ pages/yangilik.js
@@ -1,9 +1,11 @@
 import React from 'react';
+
 import YangilikSection from '../component/Yangiliklar/YangilikSection';
 
 export default function yangilik() {
   return (
     <div>
+     
         <YangilikSection/>
     </div>
   )
