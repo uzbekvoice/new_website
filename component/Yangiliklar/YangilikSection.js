2761eafbe0852b8f20709636bf5e0d89c5d97dd5
shokh001 <shohjahondilmurodov56@gmail.com>
2022-06-08 18:02:54 +0300

    news API

--- component/Yangiliklar/YangilikSection.js
+++ component/Yangiliklar/YangilikSection.js
@@ -1,26 +1,25 @@
 import React from "react";
-import data from "./YangiliklarData";
+import data2 from "./YangiliklarData";
 import Link from "next/link";
 import s from "../../styles/Yangiliklar.module.css";
 
-const YangilikSection = () => {
+const YangilikSection = ({ data }) => {
+  console.log(data, 'dd');
+
   return (
     <section className={s.YangilikSection}>
       <div className={s.container}>
         <h1 className={s.main}>Yangiliklar</h1>
-        {/* <div className={s.gradientCircle}></div>
-        <div className={s.gradientCircle2}></div>
-        <div className={s.gradientCircle3}></div> */}
         <div className={s.parent}>
-          {data.map(function (el) {
+          {data2.map(function (el) {
             return (
               <div className={s.box} key={el.id}>
                 <div className={s.boxHeader}>
-                  <img src={el.image} alt="image" key={el.id} />
+                  <img src={el.news_image} alt={el.news_image} key={el.id} />
                 </div>
                 <div className={s.boxBody}>
-                  <h3 className={s.title}>{el.title}</h3>
-                  <h5 className={s.desc}>{el.desc}</h5>
+                  <h3 className={s.title}>{el.news_title}</h3>
+                  <h5 dangerouslySetInnerHTML={{ __html: el.news_content }} className={s.desc}></h5>
                   <div className={s.flexClass}>
                     <Link href={`/yangilik/${el.id}`}>
                       <a>
@@ -28,7 +27,7 @@ const YangilikSection = () => {
                         <img src='/chevron-right.svg' />
                       </a>
                     </Link>
-                    <p className={s.ppp}>{el.date}</p>
+                    <p className={s.ppp}>{el.date_created.slice(0, 10)}</p>
                   </div>
                 </div>
               </div>
@@ -41,3 +40,5 @@ const YangilikSection = () => {
 };
 
 export default YangilikSection;
+
+
--- pages/yangilik/index.js
+++ pages/yangilik/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import YangilikSection from '../../component/Yangiliklar/YangilikSection';
 
-export default function yangilik() {
+export default function yangilik({ data }) {
+  
   return (
-    <div>     
-        <YangilikSection/>
+    <div>
+      <YangilikSection data={data.data} />
     </div>
   )
+}
+
+export async function getStaticProps() {
+
+  const res = await fetch('https://new.uzbekvoice.ai/items/news/')
+  const data = await res.json()
+  // console.log(posts);
+
+  return {
+    props: {
+      data,
+    },
+  }
 }
