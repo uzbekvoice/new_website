a60ec26676f35775849a96e30bc8120a944b40bc
Abulfayz <abulfayzmamatqulov@gmail.com>
2022-08-02 23:24:15 +0500

    Default language domain settings update

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ module.exports = {
     locales: ["uz-UZ", "en-US", "ru-RU"],
     //  default locale used when the non-locale paths are visited
     defaultLocale: "uz-UZ",
+
+    domains: [
+      {
+        domain: 'uzbekvoiceapp.vercel.app',
+        defaultLocale: 'uz-UZ',
+        http: true,
+      },
+   
+    ],
   },
 }
 
