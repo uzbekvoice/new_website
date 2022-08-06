f11eb8698a82015bfd3267f93625405aa1a3fad9
Abulfayz <abulfayzmamatqulov@gmail.com>
2022-08-06 12:51:18 +0500

    Update [yangilikById].js

--- pages/yangilik/[yangilikById].js
+++ pages/yangilik/[yangilikById].js
@@ -18,11 +18,28 @@ export default YangilikById
 //   const res = await fetch('https://admin.uzbekvoice.ai/items/news_translations/')
 //   const posts = await res.json()
 
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+  const paths = posts.data.map((post) => ({
+    params: { yangilikById: toString(post.news_id) },
+  }))
+
+  return { paths, fallback: true }
+}
+
+export async function getStaticProps({ params }) {
+=======
+>>>>>>> Stashed changes
 //   const paths = posts.data.map((post) => ({
 //     params: { yangilikById: `${post.news_id}` },
 //   }))
 //   return { paths, fallback: true }
 // }
+<<<<<<< Updated upstream
+=======
+>>>>>>> 3bbc4a10e8a7c55cd56ba209b4f2b7d6d19e552c
+>>>>>>> Stashed changes
 
 export async function getServerSideProps({ params }) {
   const res = await fetch(`https://admin.uzbekvoice.ai/items/news_translations/?filter[news_id]=${params.yangilikById}`)
