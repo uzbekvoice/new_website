eb520cf19f2b25795f373611d94c269d5a54b848
shokh001 <shohjahondilmurodov56@gmail.com>
2022-08-06 18:00:25 +0300

    partners are multilanguage

--- component/Partners.js
+++ component/Partners.js
@@ -1,32 +1,44 @@
 import React from 'react'
 import styles from '../styles/Partners.module.css'
-export default function Partners() {
-    return (
-        <div className={styles.partners}>
-            <div className={styles.partners_left}>
-                <h3 className={styles.partners_title}>Join as partners</h3>
-                <p className={styles.partners_descr}>We collect the e-mail addresses of those who communicate with us via e-mail, aggregate information on what pages consumers access or visit, and information volunteered by the consumer (such as survey information and/or site registrations). The information we collect is used to improve the content of our Web pages and the quality of our service, and is not shared with or sold to other organizations for commercial purposes, except to provide products or services you’ve requested, when we have your permission, or under the following circumstances:</p>
-            </div>
-            <form className={styles.partners_right}>
-                <label>Имя</label>
-                <input
-                    type='text'
-                    placeholder='Введите свое имя'
-                />
-                <label>Телфон номер</label>
-                <input
-                    type='number'
-                    placeholder='+998'
-                />
-                <label>Комментарий</label>
-                <textarea
-                    rows="4"
-                    cols="50"
-                    placeholder='Необязательно*'>
-                </textarea>
+import PartnersApi from '../pages/partnersapi/static.json'
+import { useRouter } from 'next/router';
 
-                <button type='button'>Отправить заявку</button>
-            </form>
-        </div>
+export default function Partners() {
+
+    const { locale } = useRouter()
+
+    return (
+        PartnersApi.partners
+            .filter((p) => p.languages_code === locale)
+            .map(({
+                id, title, guide, label_phone, label_comment, descr, label_name, placeholder_name, placeholder_comment, button
+            }) =>
+                <div className={styles.partners} key={id}>
+                    <div className={styles.partners_left}>
+                        <h3 className={styles.partners_title}>{title}</h3>
+                        <p className={styles.partners_descr}>{descr}</p>
+                    </div>
+                    <form className={styles.partners_right}>
+                        <label>{label_name}</label>
+                        <input
+                            type='text'
+                            placeholder={placeholder_name}
+                        />
+                        <label>{label_phone}</label>
+                        <input
+                            type='number'
+                            placeholder='+998'
+                        />
+                        <label>{label_comment}</label>
+                        <textarea
+                            rows="4"
+                            cols="50"
+                            placeholder={placeholder_comment}>
+                        </textarea>
+
+                        <button type='button'>{button}</button>
+                    </form>
+                </div>
+            )
     )
 }
