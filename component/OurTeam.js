d3f681cc6f9b4b3e980e2c8c4d8c4a9e3834f26b
shokh001 <shohjahondilmurodov56@gmail.com>
2022-08-06 23:20:54 +0300

    APIs connected in ourTeam component

--- component/OurTeam.js
+++ component/OurTeam.js
@@ -1,142 +1,46 @@
 import React from "react";
 import styles from "../styles/OurTeam.module.css";
-import Image from "next/image";
+import Link from 'next/link'
 import { FaTelegramPlane } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 
-export default function OurTeam() {
+export default function OurTeam({ data }) {
+  // console.log(data);
   return (
     <div className={styles.ourteam}>
       <h3>Uzbekvoice Jamoasi</h3>
       <div className={styles.flex}>
 
         <div className={styles.firstblock}>
-          {" "}
-          <div className={styles.item}>
-            <div className={styles.avatarimg}>
-              <Image
-                src="/team1.png"
-                width={150}
-                height={150}              
-                alt="partners1"
-              />
-            </div>
 
-            <h4>Courtney Henry</h4>
-            <a href="#">
-              {" "}
-              <FaLinkedin size={22} />{" "}
-            </a>
-            <a href="#">
-              {" "}
-              <FaTelegramPlane size={22} />
-            </a>
-          </div>
-          
-          <div className={styles.item}>
-            <div className={styles.avatarimg}>
-              <Image
-                src="/team3.png"
-                width={150}
-                height={150}
-              
-                alt="partners1"
-              />
-            </div>
-            <h4>Courtney Henry</h4>
+          {
+            data.map(({ id, profile_image, profile_link, profile_name }) =>
+              <div className={styles.item} key={id}>
+                <div className={styles.avatarimg}>
+                  <img
+                    src={`https://admin.uzbekvoice.ai/assets/${profile_image}`}
+                    width={150}
+                    height={150}
+                    alt="partners1"
+                  />
+                </div>
 
-            <a href="#">
-              {" "}
-              <FaLinkedin size={22} />{" "}
-            </a>
-            <a href="#">
-              {" "}
-              <FaTelegramPlane size={22} />
-            </a>
-          </div>
-          <div className={styles.item}>
-            <div className={styles.avatarimg}>
-              <Image
-                src="/team1.png"
-                width={150}
-                height={150}
-                alt="partners1"
-              />
-            </div>
-
-            <h4>Courtney Henry</h4>
-            <a href="#">
-              {" "}
-              <FaLinkedin size={22} />{" "}
-            </a>
-            <a href="#">
-              {" "}
-              <FaTelegramPlane size={22} />
-            </a>
-          </div>
-        
-        </div>
-        <div className={styles.secondblock}>
-          <div className={styles.item}>
-            <div className={styles.avatarimg}>
-              <Image
-                src="/team1.png"
-                width={150}
-                height={150}
-                alt="partners1"
-              />
-            </div>
-
-            <h4>Courtney Henry</h4>
-            <a href="#">
-              {" "}
-              <FaLinkedin size={22} />{" "}
-            </a>
-            <a href="#">
-              {" "}
-              <FaTelegramPlane size={22} />
-            </a>
-          </div>
-          <div className={styles.item}>
-            <div className={styles.avatarimg}>
-              <Image
-                src="/team3.png"
-                width={150}
-                height={150}
-                alt="partners1"
-              />
-            </div>
-            <h4>Courtney Henry</h4>
-
-            <a href="#">
-              {" "}
-              <FaLinkedin size={22} />{" "}
-            </a>
-            <a href="#">
-              {" "}
-              <FaTelegramPlane size={22} />
-            </a>
-          </div>
-          <div className={styles.item}>
-            <div className={styles.avatarimg}>
-              <Image
-                src="/team3.png"
-                width={150}
-                height={150}
-                alt="partners1"
-              />
-            </div>
-            <h4>Courtney Henry</h4>
-
-            <a href="#">
-              {" "}
-              <FaLinkedin size={22} />{" "}
-            </a>
-            <a href="#">
-              {" "}
-              <FaTelegramPlane size={22} />
-            </a>
-          </div>
+                <h4>{profile_name}</h4>
+                <div>
+                  <Link href={profile_link}>
+                    <a className={styles.FaLinkedin}>
+                      <FaLinkedin size={22} />
+                    </a>
+                  </Link>
+                  <Link href='/'>
+                    <a>
+                      <FaTelegramPlane size={22} />
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            )
+          }
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -14,24 +14,28 @@ import HomeContent from './homeapi/static.json'
 export async function getStaticProps() {
   const res = await fetch("https://commonvoice.mozilla.org/api/v1/uz/clips/leaderboard");
   const res2 = await fetch("https://api.ry.team/leaderboard/votes/all");
-  const res3 = await fetch('https://admin.uzbekvoice.ai/items/partners')
+  const res3 = await fetch('https://admin.uzbekvoice.ai/items/partners');
+  const res4 = await fetch('https://admin.uzbekvoice.ai/items/team_members');
+
 
   const data = await res.json();
   const users = await res2.json();
   const partners = await res3.json();
+  const teamMembers = await res4.json();
 
   return {
     props: {
       users: data,
       userslist: users,
-      partners
+      partners,
+      teamMembers
     },
 
   };
 }
 
-export default function Home({ users, userslist, partners }) {
-
+export default function Home({ users, userslist, partners, teamMembers }) {
+  
   return (
     <div>
       <Hero HomeContent={HomeContent} />
@@ -42,7 +46,7 @@ export default function Home({ users, userslist, partners }) {
       <TopUser users={users} userslist={userslist} HomeContent={HomeContent} />
       <Ourpartners HomeContent={HomeContent} />
       <InitiativePartners HomeContent={HomeContent} partners={partners.data} />
-      <OurTeam />
+      <OurTeam data={teamMembers.data} />
       <Question />
     </div>
   );
