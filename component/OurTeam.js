c582a2663c0e1797878bcc39dfa27244fb928449
shokh001 <shohjahondilmurodov56@gmail.com>
2022-08-07 12:43:04 +0300

    ourteam responsive is fixed

--- component/OurTeam.js
+++ component/OurTeam.js
@@ -18,40 +18,35 @@ export default function OurTeam({ data, HomeContent }) {
           return (
             <div key={i}>
               <h3>{title}</h3>
-              
             </div>
           );
         })}
 
-      <div className={styles.flex}>
-        <div className={styles.firstblock}>
-          {data.map(({ id, profile_image, profile_link, profile_name }) => (
-            <div className={styles.item} key={id}>
-              <div className={styles.avatarimg}>
-                <img
-                  src={`https://admin.uzbekvoice.ai/assets/${profile_image}`}
-                  width={150}
-                  height={150}
-                  alt="partners1"
-                />
-              </div>
-
-              <h4>{profile_name}</h4>
-              <div>
-                <Link href={profile_link}>
-                  <a className={styles.FaLinkedin}>
-                    <FaLinkedin size={22} />
-                  </a>
-                </Link>
-                <Link href="/">
-                  <a>
-                    <FaTelegramPlane size={22} />
-                  </a>
-                </Link>
-              </div>
+      <div className={styles.firstblock}>
+        {data.map(({ id, profile_image, profile_link, profile_name }) => (
+          <div className={styles.item} key={id}>
+            <div className={styles.avatarimg}>
+              <img
+                src={`https://admin.uzbekvoice.ai/assets/${profile_image}`}
+                alt="partners1"
+              />
             </div>
-          ))}
-        </div>
+
+            <h4>{profile_name}</h4>
+            <div>
+              <Link href={profile_link}>
+                <a className={styles.FaLinkedin}>
+                  <FaLinkedin size={22} />
+                </a>
+              </Link>
+              <Link href="/">
+                <a>
+                  <FaTelegramPlane size={22} />
+                </a>
+              </Link>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
