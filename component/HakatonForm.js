29a805a93d96123025d079b4aed54cda0a64fd50
Abulfayz <abulfayzmamatqulov@gmail.com>
2022-08-24 19:21:33 +0500

    added NexSeo

--- component/HakatonForm.js
+++ component/HakatonForm.js
@@ -178,7 +178,6 @@ export default function Partners() {
                 <input
                   value={dateOfBirth}
                   type="date"
-                
                   required
                   
                   // placeholder={""}
