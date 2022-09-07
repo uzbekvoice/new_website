108cc89a6494ab3639c3d824dc48cb9f6fa6ffd4
Abulfayz <abulfayzmamatqulov@gmail.com>
2022-09-07 18:21:13 +0500

    test connect local directus

--- next.config.js
+++ next.config.js
@@ -8,6 +8,17 @@ module.exports = {
     localeDetection: false   
   },
 
+  publicRuntimeConfig: {
+    url: process.env.DIRECTUS_URL,
+  },
+  serverRuntimeConfig: {
+    email: process.env.DIRECTUS_EMAIL,
+    password: process.env.DIRECTUS_PASSWORD,
+    // token: process.env.DIRECTUS_STATIC_TOKEN,
+  },
+
+
+
   async redirects() {
     return [
       {
--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import Question from "../component/Question/Question";
 import Step from "../component/Step";
 import TopUser from "../component/TopUser";
 import dynamic from "next/dynamic";
+import { getDirectusClient } from "../lib/directus";
 const Stat = dynamic(
   () => import('../component/Stat'),
   { ssr: false }
@@ -16,10 +17,15 @@ const Stat = dynamic(
 import HomeContent from './homeapi/static.json'
 
 export async function getServerSideProps() {
+
+  const directus = await getDirectusClient();
+  const response = await directus.items("team_members_translations").readByQuery({
+    fields: ["*"],
+  })
   const res = await fetch("https://commonvoice.mozilla.org/api/v1/uz/clips/leaderboard");
   const res2 = await fetch("https://api.ry.team/leaderboard/votes/all");
   const res3 = await fetch('https://admin.uzbekvoice.ai/items/partners');
-  const res4 = await fetch('https://admin.uzbekvoice.ai/items/team_members_translations');
+  // const res4 = await fetch('https://admin.uzbekvoice.ai/items/team_members_translations');
   const res5 = await fetch('https://admin.uzbekvoice.ai/items/faq_translations');
   const res6 = await fetch('https://admin.uzbekvoice.ai/items/contest_stages_translations');
 
@@ -27,7 +33,7 @@ export async function getServerSideProps() {
   const data = await res.json();
   const users = await res2.json();
   const partners = await res3.json();
-  const teamMembers = await res4.json();
+  // const teamMembers = await res4.json();
   const faq = await res5.json();
   const steps = await res6.json();
 
@@ -36,7 +42,7 @@ export async function getServerSideProps() {
       users: data,
       userslist: users,
       partners,
-      teamMembers,
+      response,
       faq,
       steps
     },
@@ -44,7 +50,7 @@ export async function getServerSideProps() {
   };
 }
 
-export default function Home({ users, userslist, partners, teamMembers, faq, steps }) {
+export default function Home({ users, userslist, partners, response, faq, steps }) {
 
   return (
       <div>
@@ -56,7 +62,7 @@ export default function Home({ users, userslist, partners, teamMembers, faq, ste
         <TopUser users={users} userslist={userslist} HomeContent={HomeContent} />
         <Ourpartners HomeContent={HomeContent} />
         <InitiativePartners HomeContent={HomeContent} partners={partners.data} />
-        <OurTeam data={teamMembers.data} HomeContent={HomeContent} />
+        <OurTeam data={response.data} HomeContent={HomeContent} />
         <Question data={faq.data} HomeContent={HomeContent} />
       </div>
   );
