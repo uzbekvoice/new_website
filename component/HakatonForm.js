22f696e529698549a5d837afcc00b6ce36353bf3
Abulfayz <abulfayzmamatqulov@gmail.com>
2022-09-19 13:45:55 +0500

    Recaptcha site key changed

--- component/HakatonForm.js
+++ component/HakatonForm.js
@@ -369,8 +369,8 @@ export default function Partners() {
                 </label>
               </div>
               <ReCAPTCHA
-    // sitekey="6LdOKLEhAAAAAKPMaI1f5RrNqiedwS0Rw9AePzSg" localhost
-    sitekey="6LcJc7EhAAAAAH0x_sVS2Ai9EwXpeQkwaFN3sE7d"
+   
+    sitekey={process.env.REACT_APP_SITE_KEY}
     onChange={onChange}
   />,
 
--- component/Partners.js
+++ component/Partners.js
@@ -114,8 +114,8 @@ export default function Partners() {
 
            
             <ReCAPTCHA 
-            //   sitekey="6LdOKLEhAAAAAKPMaI1f5RrNqiedwS0Rw9AePzSg" localhost
-            sitekey="6LcJc7EhAAAAAH0x_sVS2Ai9EwXpeQkwaFN3sE7d"
+           
+            sitekey={process.env.REACT_APP_SITE_KEY}
               onChange={onChange}
               
             />
