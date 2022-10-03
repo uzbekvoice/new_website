352a610a09112c28c04ba8652374c4d16cacc66b
Abulfayz <abulfayzmamatqulov@gmail.com>
2022-10-03 22:30:45 +0500

    Sitemap added

---
+++ next-sitemap.config.js
@@ -0,0 +1,14 @@
+const siteUrl = "https://uzbekvoice.ai";
+module.exports = {
+  siteUrl,
+  generateRobotsTxt: true, // (optional)
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+
+  },
+};
