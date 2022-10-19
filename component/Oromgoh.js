ea4837f66320ac5c594c02c5ba7a1528557009e1
Abulfayz <abulfayzmamatqulov@gmail.com>
2022-10-19 19:42:39 +0500

    Chnge logo background oromgoh  prizes

--- component/Oromgoh.js
+++ component/Oromgoh.js
@@ -5,38 +5,44 @@ import BusAnimation from "./BusAnimation";
 
 export default function Oromgoh() {
   return (
+    <div>
+
+
     <div className={styles.block}>
       <h1>Oromgohda qatnashing</h1>
 
       <div className={styles.flex}>
         <div>
-          <h3>Boshlanishi</h3>
+          <h3>Start</h3>
           <h4>21 Oktyabr</h4>
         </div>
 
         <div>
-          <h3>Tugalanishi</h3>
-          <h4>15 Noyabr</h4>
+          <h3>Finish</h3>
+          <h4>1 Dekabr</h4>
         </div>
       </div>
 
     <BusAnimation />
-      <div className={styles.desc}>
-        <ul>
-          <li>
-            <span>&#10003; </span> Tabiatda dam olish
-          </li>
-          <li><span>&#10003; </span> O'yin kulgu</li>
-          <li><span>&#10003; </span> Tekin ovqat</li>
-          <li><span>&#10003; </span> va albatta ovozli ma'lumotlar yozish</li>
-        </ul>
-        <div className={styles.btn}>
-            <Link href="#">
-            <a >Ishtirok etish</a>
-            </Link>
-          
-        </div>
-      </div>
+ 
     </div>
+
+<div className={styles.desc}>
+<ul>
+  <li>
+    <span>&#10003; </span> Tabiatda dam olish
+  </li>
+  <li><span>&#10003; </span> O'yin kulgu</li>
+  <li><span>&#10003; </span> Tekin ovqat</li>
+  <li><span>&#10003; </span> va albatta ovozli ma'lumotlar yozish</li>
+</ul>
+<div className={styles.btn}>
+    <Link href="#">
+    <a >Ishtirok etish</a>
+    </Link>
+  
+</div>
+</div>
+</div>
   );
 }
