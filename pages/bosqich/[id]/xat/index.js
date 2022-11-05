43380f85e525744657a0a8a2bc613c4b59794b83
Abulfayz <abulfayzmamatqulov@gmail.com>
2022-11-05 14:52:59 +0500

    download file

---
+++ pages/bosqich/[id]/xat/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Xat() {
+  return (
+    <div className='xat'>
+
+        <a href="/users.pdf" className='btn' download>Yuklab olish </a>
+    </div>
+  )
+}
