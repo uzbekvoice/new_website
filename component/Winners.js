8721130c7a47f00043e6060271cd739eecd65d1f
Javohir Hakimov <uzwebprogrammer@gmail.com>
2022-11-23 23:02:51 +0500

    updates

--- component/Winners.js
+++ component/Winners.js
@@ -35,7 +35,7 @@ export default function Winners({ users, userslist, bosqich }) {
             }
           </h4>
           {
-            audioListener.map((post) =>
+            audioListener.slice(0, 3).map((post) =>
               <div key={post.id} className={styles.card}>
                 <div className={styles.item}>
                   <div className={styles.block}>
@@ -71,7 +71,7 @@ export default function Winners({ users, userslist, bosqich }) {
             }
           </h4>
           {
-            audioChecker.map((post) =>
+            audioChecker.slice(0, 3).map((post) =>
               <div key={post.id} className={styles.card}>
                 <div className={styles.item}>
                   <div className={styles.block}>
@@ -98,47 +98,45 @@ export default function Winners({ users, userslist, bosqich }) {
           {/* More voice top user */}
           <div className={styles.voice_user}>
             {showModal ? (
-          
-                <div onClick={() => setShowModal(false)} className={styles.modal_bg}>
-                  <div onClick={e => e.stopPropagation()} className={styles.modal_content}>
-                    <div className={styles.modal_heading}>
-                      <div className={styles.modal_close}>
-                        <button onClick={() => setShowModal(false)}>
-                          <span>
-                            <FiXCircle size={30} />
-                          </span>
-                        </button>
-                      </div>
-                      <div className={styles.title}>
-                        <h3> {
-                          locale === "uz-UZ" ?
-                            <span>Eng yaxshi ovoz tekshiruvchi.</span>
-                            : locale === "ru-RU" ?
-                              <span>Активные участники</span>
-                              : <span>The most active listener</span>
-                        }</h3>
-                      </div>
 
-                      {/*body*/}
-                      <div className={styles.container}>
-                        {users.map((dates) => (
-                          <div key={dates.clientHash} className={styles.card_user}>
-                            <div className={styles.item_user}>
-                              <div className={styles.block_user}>
-                                <h4>{dates.position + 1}</h4>
-                                <h5>{dates.username}</h5>
-                              </div>
-                              <span>{dates.total}</span>
+              <div onClick={() => setShowModal(false)} className={styles.modal_bg}>
+                <div onClick={e => e.stopPropagation()} className={styles.modal_content}>
+                  <div className={styles.modal_heading}>
+                    <div className={styles.modal_close}>
+                      <button onClick={() => setShowModal(false)}>
+                        <span>
+                          <FiXCircle size={30} />
+                        </span>
+                      </button>
+                    </div>
+                    <div className={styles.title}>
+                      <h3> {
+                        locale === "uz-UZ" ?
+                          <span>Eng yaxshi ovoz tekshiruvchi.</span>
+                          : locale === "ru-RU" ?
+                            <span>Активные участники</span>
+                            : <span>The most active listener</span>
+                      }</h3>
+                    </div>
+
+                    {/*body*/}
+                    <div className={styles.container}>
+                      {audioListener.map((dates) => (
+                        <div key={dates.id} className={styles.card_user}>
+                          <div className={styles.item_user}>
+                            <div className={styles.block_user}>
+                              <h4>{dates.id}</h4>
+                              <h5>{dates.name}</h5>
                             </div>
+                            <span>{dates.ranking}</span>
                           </div>
-
-                        ))}
-
-                      </div>
+                        </div>
+                      ))}
                     </div>
                   </div>
                 </div>
-           
+              </div>
+
             ) : null}
           </div>
           {/* //More voice top user */}
@@ -146,46 +144,45 @@ export default function Winners({ users, userslist, bosqich }) {
           {/* More listening top user */}
           <div className={styles.voice_user}>
             {showModal2 ? (
-           
-                <div onClick={() => setShowModal2(false)} className={styles.modal_bg}>
-                  <div onClick={e => e.stopPropagation()} className={styles.modal_content}>
-                    <div className={styles.modal_heading}>
-                      <div className={styles.modal_close}>
-                        <button onClick={() => setShowModal2(false)}>
-                          <span>
-                            <FiXCircle size={30} />
-                          </span>
-                        </button>
-                      </div>
-                      <div className={styles.title}>
-                        <h3>{
-                          locale === "uz-UZ" ?
-                            <span>Eng yaxshi ovoz yozuvchi</span>
-                            : locale === "ru-RU" ?
-                              <span>Самый активный спикер</span>
-                              : <span>The most active speaker</span>
-                        }</h3>
-                      </div>
 
-                      {/*body*/}
-                      <div className={styles.container}>
-                        {userslist.map((data) => (
-                          <div key={data.clientHash} className={styles.card_user}>
-                            <div className={styles.item_user}>
-                              <div className={styles.block_user}>
-                                <h4>{data.position + 1}</h4>
-                                <h5>{data.username}</h5>
-                              </div>
-                              <span>{data.total}</span>
+              <div onClick={() => setShowModal2(false)} className={styles.modal_bg}>
+                <div onClick={e => e.stopPropagation()} className={styles.modal_content}>
+                  <div className={styles.modal_heading}>
+                    <div className={styles.modal_close}>
+                      <button onClick={() => setShowModal2(false)}>
+                        <span>
+                          <FiXCircle size={30} />
+                        </span>
+                      </button>
+                    </div>
+                    <div className={styles.title}>
+                      <h3>{
+                        locale === "uz-UZ" ?
+                          <span>Eng yaxshi ovoz yozuvchi</span>
+                          : locale === "ru-RU" ?
+                            <span>Самый активный спикер</span>
+                            : <span>The most active speaker</span>
+                      }</h3>
+                    </div>
+
+                    {/*body*/}
+                    <div className={styles.container}>
+                      {audioChecker.map((dates) => (
+                        <div key={dates.id} className={styles.card_user}>
+                          <div className={styles.item_user}>
+                            <div className={styles.block_user}>
+                              <h4>{dates.id}</h4>
+                              <h5>{dates.name}</h5>
                             </div>
+                            <span>{dates.ranking}</span>
                           </div>
-
-                        ))}
-                      </div>
+                        </div>
+                      ))}
                     </div>
                   </div>
                 </div>
-           
+              </div>
+
             ) : null}
           </div>
           {/* //More listening top user */}
