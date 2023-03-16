5173f504f64201091be2b8d13c393c1f21d4e16c
Javohir Hakimov <uzwebprogrammer@gmail.com>
2023-03-16 16:28:18 +0500

    update dataset

--- component/Header.js
+++ component/Header.js
@@ -40,6 +40,7 @@ function Header({ menuToggle, setMenuToggle, openLang, setOpenLang }) {
         hakatonlar,
         roadmap,
         qollanma,
+        dataset,
         boshqa,
         boshqalar,
         button,
@@ -201,6 +202,17 @@ function Header({ menuToggle, setMenuToggle, openLang, setOpenLang }) {
                   }
                 >
                   {qollanma}
+                </a>
+              </Link>
+              <Link href="https://drive.google.com/drive/folders/18N5i7GD0LmUnNQok6BP3EC8PYov7pZDW">
+                <a
+                  onClick={() => setMenuToggle(false)}
+                  target="_blank"
+                  className={
+                    router.pathname == "https://drive.google.com/drive/folders/18N5i7GD0LmUnNQok6BP3EC8PYov7pZDW" ? "item active" : "item"
+                  }
+                >
+                  {dataset}
                 </a>
               </Link>
               <div
